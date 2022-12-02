eac91d9a351c2d42e54a4e07d769fa0487ff21b3
yamashi4423 <yamashi4423@gmail.com>
2022-12-03 02:19:03 +0900

    C:/Program Files/Git/about#近況wo

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import Article from "../components/articles";
 import Main from "../components/Main";
 import Subtitle from "../components/Subtitle";
 import Title from "../components/Title";
@@ -9,15 +10,25 @@ function about() {
   return (
     <Main>
       <Title content="わたしについて" />
-      <Subtitle content="なまえ" />
-      <p>「やかん」です</p>
-      <Subtitle content="身分" />
-      <p>
-        大学三年生です，就職したくないので大学院にいければいいなって思ってます
-      </p>
-      <Subtitle content="趣味" />
-      <p>アニメとか漫画とか</p>
-      <p>最近はHARTAコミックスと和山やまさんとかすきです</p>
+      <Article>
+        <Subtitle content="なまえ" />
+        <p>「やかん」です</p>
+      </Article>
+      <Article>
+        <Subtitle content="身分" />
+        <p>
+          大学三年生です，就職したくないので大学院にいければいいなって思ってます
+        </p>
+      </Article>
+      <Article>
+        <Subtitle content="趣味" />
+        <p>アニメとか漫画とか</p>
+        <p>最近はHARTAコミックスと和山やまさんとかすきです</p>
+      </Article>
+      <Article>
+        <Subtitle content="近況" />
+        <p>Next.jsをはじめました．おもしろいです．</p>
+      </Article>
     </Main>
   );
 }
